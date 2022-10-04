90fda66133ee62113fe14dbf79a04332ad0209eb
Nici-Shad <80696922+Nici-Shad@users.noreply.github.com>
2022-10-04 14:37:40 +0100

    added more story text in text.py file and code in run.py file

--- run.py
+++ run.py
@@ -1,15 +1,19 @@
 """
 Start of the Project and File
 """
-import imp
 import os
+import sys
 from tkinter.tix import TEXT  # To clear the console
 import datetime  # To get the hour of the day
 import time  # To give a delay when typing
 import colorama # Import Colors into project
 from colorama import Fore
+from colored import fg, bg, attr
 from text import *
 colorama.init()
+
+#Global Var for the username, age and location
+USERNAME = ""
 
 def main_menu():
     """
@@ -17,19 +21,126 @@ def main_menu():
     """
     print(TITLE)
     print(MAIN_TEXT)
+
     while True:
         answer = input(
             f"To start The game, type {Fore.BLUE}start{Fore.WHITE}. "
             f"If you want to know more about the game and how to play this game"
-            f"type {Fore.BLUE} Info/Information{Fore.WHITE} "
-            f"If you want to exit the application, just type {Fore.BLUE}exit{Fore.WHITE} "
+            f"type {Fore.BLUE}Info/Information{Fore.WHITE} "
+            f"If you want to exit the application, just type {Fore.BLUE}exit{Fore.WHITE}\n" +
+            "Answer: "
         )
-        if answer == "exit" or "i want to leave" or "let me out":
+        if answer == "Start" or answer == "start" or answer == "START":
+            start_game()
+        elif answer == "exit" or answer == "EXIT" or answer == "Exit":
             exit_app()
-        elif answer == "info" or "information":
+        elif answer == "info" or answer == "information" or answer == "Info" or answer == "Information":
             information_menu()
+        else:
+            wrong_input(answer)
 
-#Helper Functions
+def get_username():
+    """
+    This function will retreive the username that the user will enter.
+    """
+    while True:
+        name = input("What should I call you?\n" + "Answer: ").upper().strip()
+        if len(name) < 2:
+            print("The Length of the username is too short, please try again")
+        elif len(name) > 15:
+            print("The username is too long, try a shorter username.")
+        elif any(char.isdigit() for char in name):
+            print("The username cannot contain any numbers.")
+        elif all(char.isalpha() or char.isspace() for char in name):
+            global USERNAME
+            USERNAME = " ".join(name.split()).title()
+            break
+        else:
+            print("Your name cannot contain any symbols, please try again.")
+
+def start_game():
+    """
+    When the user answers 'start' this function will run and the user
+    can start playing the game.
+    """
+    clear()
+    type_delay("Game is starting...\n")
+    get_username()
+
+    print(ROOM_DESIGN)
+    type_delay(USERNAME + STORY_START)
+
+    while True:
+        # MULTIPLE TERMS AS KBROTHER WHEN HE COMES BACK
+        # MULTIPLE TERMS AS KBROTHER WHEN HE COMES BACK
+        # MULTIPLE TERMS AS KBROTHER WHEN HE COMES BACK
+        # MULTIPLE TERMS AS KBROTHER WHEN HE COMES BACK
+        term = "look"
+        answer = input("Answer: ").lower()
+        words = answer.split()
+
+        if term in words:
+            type_delay(f"Where do you look first?\n"
+            f"Forward{Fore.BLUE}(NORTH){Fore.WHITE}, "
+            f"To Your Right{Fore.GREEN}(East){Fore.WHITE}, "
+            f"Behind You{Fore.RED}(South){Fore.WHITE} "
+            f"or to Your Left{Fore.YELLOW}(West){Fore.WHITE}\n")
+            look_answer = input("Answer: ").lower()
+            # When the user look to the North
+            if look_answer in ("north", "n", "forward"):
+                clear()
+                print(ROOM_DESIGN_NORTH)
+                type_delay(f"{Fore.BLUE}{look_answer}:{Fore.WHITE}" +
+                " You see a door… What do you do?\n")
+                north_answer = input("Asnwer: ").lower()
+                # MULTIPLE TERMS AS KBROTHER WHEN HE COMES BACK
+                # MULTIPLE TERMS AS KBROTHER WHEN HE COMES BACK
+                # MULTIPLE TERMS AS KBROTHER WHEN HE COMES BACK
+                # MULTIPLE TERMS AS KBROTHER WHEN HE COMES BACK
+
+                # When the user inspect the door
+                term_door = "inspect"
+                words_inspect = north_answer.split()
+                term_door_approach = "approach"
+                words_approach = north_answer.split()
+                term_door_open = "open"
+                words_open = north_answer.split()
+                if  term_door in words_inspect:
+                    type_delay(STORY_NORTH_INSPECT)
+                    
+                # When the user approach the door
+                elif term_door_approach in words_approach:
+                    type_delay(STORY_NORTH_APPROACH)
+                # When the user open the door
+                elif term_door_open in words_open:
+                    type_delay(STORY_NORTH_OPEN)
+                elif north_answer == "back":
+                    print(start_game)
+                else:
+                    print(wrong_input)
+
+            # When the user look to the East
+            elif look_answer in ("east", "e", "right"):
+                clear()
+                print(ROOM_DESIGN_EAST)
+                type_delay(f"{Fore.GREEN}{look_answer}:{Fore.WHITE}" +
+                "You Turn to your right and see a big bookshelf that is almost as wide as the wall. "
+                "What do you do?\n")
+            # When the user look to the South
+            elif look_answer in ("south", "s", "behind", "behind me", "turn around"):
+                clear()
+                print(ROOM_DESIGN_SOUTH)
+                type_delay(f"{Fore.RED}{look_answer}:{Fore.WHITE}" +
+                "You See a table with two chairs on each side. What do you do?\n")
+            # When the user look to the West
+            elif look_answer in ("west", "w", "left"):
+                clear()
+                print(ROOM_DESIGN_WEST)
+                type_delay(f"{Fore.YELLOW}{look_answer}:{Fore.WHITE}" +
+                "You turn to your left, you see a desk. What do you do?\n")
+            else:
+                print(wrong_input)
+# Helper Functions
 def clear():
     """
     This will clear the terminal
@@ -62,21 +173,25 @@ def type_delay(text):
     when certain text is displaying.
     """
     for char in text:
-        system.stdout.write(char)
-        system.stdout.flush()
-        time.sleep(.2)
-    time.sleep(3)
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(.05)
+    time.sleep(1)
 
 def wrong_input(answer):
     """
     When the user gives an invalid input, the function will be called
     """
-    print(f"\t{Fore.RED}Wrong command: {Fore.WHITE}'{answer}'"
-    f"Please enter a valid command.\n"
+    print(f"\t{Fore.RED}Wrong command: {Fore.WHITE}'{answer}' is not a valid command."
+    f"Please try again.\n"
     )
 
 def information_menu():
     """
-    
+    The details on how to play the game will be displayed.
     """
+    clear()
+    print(INFORMATION_TEXT)
+
+
 main_menu()
--- text.py
+++ text.py
@@ -18,14 +18,35 @@ ________________________________________________________________________________
 ______________________________________________________________________________________________                                                                                        
 """
 
-ROOM_DESIGN = """
+ROOM_DESIGN = f"""
  ________________________________________
 |             |___________|              |
 |                                        |
 |                                     ___|
 |                                    |   |
 |  _____                             |   |
-| |     |           N                |   |
+| |     |           {Fore.BLUE}N{Fore.WHITE}                |   |
+| |     |         {Fore.YELLOW}W{Fore.WHITE} + {Fore.GREEN}E{Fore.WHITE}              |   |
+| |     |           {Fore.RED}S{Fore.WHITE}                |   |
+| |_____|                            |   |
+|                                    |   |
+|                                    |   |
+|               __________           |   |
+|              |          |          |   |
+|           |  |          |  |       |___|
+|           |  |          |  |           |
+|______________|__________|______________|
+"""
+
+ROOM_DESIGN_NORTH = f"""
+{Fore.BLUE}
+ ________________________________________
+|             |___________|              |
+|                                        |
+|                                     ___| {Fore.WHITE}
+|                                    |   |
+|  _____                             |   |
+| |     |           {Fore.BLUE}N{Fore.WHITE}                |   |
 | |     |         W + E              |   |
 | |     |           S                |   |
 | |_____|                            |   |
@@ -38,6 +59,66 @@ ROOM_DESIGN = """
 |______________|__________|______________|
 """
 
+ROOM_DESIGN_EAST = f"""
+_____________________________________{Fore.GREEN}___{Fore.WHITE}
+|             |___________|          {Fore.GREEN}    |{Fore.WHITE}
+|                                    {Fore.GREEN}    |{Fore.WHITE}
+|                                    {Fore.GREEN} ___|{Fore.WHITE}
+|                                    {Fore.GREEN}|   |{Fore.WHITE}
+|  _____                             {Fore.GREEN}|   |{Fore.WHITE}
+| |     |           N                {Fore.GREEN}|   |{Fore.WHITE}
+| |     |         W + {Fore.GREEN}E{Fore.WHITE}              {Fore.GREEN}|   |{Fore.WHITE}
+| |     |           S                {Fore.GREEN}|   |{Fore.WHITE}
+| |_____|                            {Fore.GREEN}|   |{Fore.WHITE}
+|                                    {Fore.GREEN}|   |{Fore.WHITE}
+|                                    {Fore.GREEN}|   |{Fore.WHITE}
+|               __________           {Fore.GREEN}|   |{Fore.WHITE}
+|              |          |          {Fore.GREEN}|   |{Fore.WHITE}
+|           |  |          |  |       {Fore.GREEN}|___|{Fore.WHITE}
+|           |  |          |  |       {Fore.GREEN}    |{Fore.WHITE}
+|______________|__________|__________{Fore.GREEN}____|{Fore.WHITE}
+"""
+
+ROOM_DESIGN_SOUTH = f"""
+ ________________________________________
+|             |___________|              |
+|                                        |
+|                                     ___|
+|                                    |   |
+|  _____                             |   |
+| |     |           N                |   |
+| |     |         W + E              |   |
+| |     |           {Fore.RED}S{Fore.WHITE}                |   |
+| |_____|                            |   |
+|                                    |   |
+|                                    |   |
+{Fore.RED}|               __________           |   |
+|              |          |          |   |
+|           |  |          |  |       |___|
+|           |  |          |  |           |
+|______________|__________|______________| {Fore.WHITE}
+"""
+
+ROOM_DESIGN_WEST = f"""
+{Fore.YELLOW} __________{Fore.WHITE}______________________________
+{Fore.YELLOW}|         {Fore.WHITE}    |___________|              |
+{Fore.YELLOW}|         {Fore.WHITE}                               |
+{Fore.YELLOW}|         {Fore.WHITE}                            ___|
+{Fore.YELLOW}|         {Fore.WHITE}                           |   |
+{Fore.YELLOW}|  _____  {Fore.WHITE}                           |   |
+{Fore.YELLOW}| |     | {Fore.WHITE}          N                |   |
+{Fore.YELLOW}| |     | {Fore.WHITE}        {Fore.YELLOW}W{Fore.WHITE} + E              |   |
+{Fore.YELLOW}| |     | {Fore.WHITE}          S                |   |
+{Fore.YELLOW}| |_____| {Fore.WHITE}                           |   |
+{Fore.YELLOW}|         {Fore.WHITE}                           |   |
+{Fore.YELLOW}|         {Fore.WHITE}                           |   |
+{Fore.YELLOW}|         {Fore.WHITE}      __________           |   |
+{Fore.YELLOW}|         {Fore.WHITE}     |          |          |   |
+{Fore.YELLOW}|         {Fore.WHITE}  |  |          |  |       |___|
+{Fore.YELLOW}|         {Fore.WHITE}  |  |          |  |           |
+{Fore.YELLOW}|_________{Fore.WHITE}_____|__________|______________|
+"""
+
 MAIN_TEXT = """
 Welcome to the Escape Room. This game will test your problem solving 
 skills and creativity. You will be dropped in a room and you will have to 
@@ -45,5 +126,119 @@ think of different commands to get yourself out of the room.
 """
 
 INFORMATION_TEXT = """
+  _____ _   _ ______ ____  _____  __  __       _______ _____ ____  _   _ 
+ |_   _| \ | |  ____/ __ \|  __ \|  \/  |   /\|__   __|_   _/ __ \| \ | |
+   | | |  \| | |__ | |  | | |__) | \  / |  /  \  | |    | || |  | |  \| |
+   | | | . ` |  __|| |  | |  _  /| |\/| | / /\ \ | |    | || |  | | . ` |
+  _| |_| |\  | |   | |__| | | \ \| |  | |/ ____ \| |   _| || |__| | |\  |
+ |_____|_| \_|_|    \____/|_|  \_\_|  |_/_/    \_\_|  |_____\____/|_| \_|
+___________________________________________________________________________
 
+Want to learn how to play the Escape Room game? Well it is very simple and easy. 
+You will be given a scenario and you can give a command to further the story. The command 
+can be anything related to the scenario. For Example: The scenario is as follow...\n
+You find yourself in a car... What do you do?\n
+
+You can answer anything down the line like:
+Answer: Start the car\n
+Answer: Look around\n
+and so on.\n
+
+Depending on your answer, the story will progress accordingly.
+"""
+
+STORY_START = """
+You find yourself in a small room. You feel foggy and you dont know where you are or how you got here. 
+You get the feeling that you need to get out of this room...\n
+What do you do?
+"""
+
+STORY_NORTH_INSPECT = f"""
+You see that there is a \033[5;35mPurple\033[0;0m combination padlock on the door, 
+it seems like it needs a \033[5;35m4 digit\033[0;0m code to unlock. Do you want to unlock the door? (Y/N)
+"""
+
+STORY_NORTH_APPROACH = f"""
+As you get closer to the door, you notice a \033[5;35mPurple\033[0;0m combination padlock on the door, 
+it seems like you would need a \033[5;35m4 digit\033[0;0m code to unlock. Do you want to unlock the door? (Y/N)
+"""
+
+STORY_NORTH_OPEN = f"""
+As you reached out to the door handle to open the door, you notice a \033[5;35mPurple\033[0;0m combination padlock. 
+You first have to enter a \033[5;35m4 digit\033[0;0m code. Do you want to unlock the door? (Y/N)
+
+"""
+
+TIPS_1 = """
+Life Tip:
+The greatest glory in living lies not in never falling, but in rising every time we fall.
+
+- Nelson Mandela
+"""
+
+TIPS_2 = """
+Life Tip:
+The way to get started is to quit talking and begin doing. 
+
+- Walt Disney
+"""
+
+TIPS_3 = """
+Life Tip:
+Your time is limited, so don't waste it living someone else's life. 
+Don't be trapped by dogma, which is living with the results of other people's thinking. 
+
+- Steve Jobs
+"""
+
+TIPS_4 = """
+Life Tip:
+If life were predictable it would cease to be life, and be without flavor. 
+
+- Eleanor Roosevelt
+"""
+
+TIPS_5 = """
+Life Tip:
+If you look at what you have in life, you'll always have more. 
+If you look at what you don't have in life, you'll never have enough. 
+
+- Oprah Winfrey
+"""
+
+TIPS_6 = """
+Life Tip:
+If you set your goals ridiculously high and it's a failure, 
+you will fail above everyone else's success. 
+
+- James Cameron
+
+"""
+
+TIPS_7 = """
+Life Tip:
+Life is what happens when you're busy making other plans. 
+
+- John Lennon
+"""
+
+TIPS_8 = """
+Life Tip:
+Spread love everywhere you go. Let no one ever come to you without leaving happier. 
+
+- Mother Teresa
+"""
+
+TIPS_9 = """
+Life Tip:
+Always remember that you are absolutely unique. Just like everyone else. 
+
+- Margaret Mead
+"""
+
+TIPS_10 = """
+Life Tip:
+Don't judge each day by the harvest you reap but by the seeds that you plant. 
+
+- Robert Louis Stevenson
 """
